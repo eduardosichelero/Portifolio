4d465959f335b605274394d2c60b2b0167500fe6
Eduardo da Silva Sichelero <135671476+eduardosichelero@users.noreply.github.com>
2023-10-25 10:55:24 -0300

    Implementação dos scripts e cores

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+
+
+
+function ativaLetra(elemento) {
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((letra, i)=> {
+        setTimeout (()=>{
+            elemento.innerHTML += letra;
+        }, 75 * i)
+    })
+}
+const titulo = document.querySelector('.digitando')
+ativaLetra(titulo);
+
+// Rolagem
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('.header')
+    header.classList.toggle('rolagem-head',window.scrollY > 500)
+})
+
+
+const hamburguer = document.querySelector("hamburguer");
+const nav = document.querySelector(".navbar");
+
+hamburguer.addEventListener("click", () => nav.classList.toggle("active"))
